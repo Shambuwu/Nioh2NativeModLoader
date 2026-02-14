ab7d42620ed1e7bf81c14118430c9a9adf8d8501
Shambuwu <balfappius@protonmail.ch>
2026-02-14 02:56:00 +0100

    Add native startup overlay indicator

--- dinput8/dllmain.cpp
+++ dinput8/dllmain.cpp
@@ -26,7 +26,23 @@ std::wstring Now() {
     return buf;
 }
 
-// process-wide single init guard
+HWND FindGameWindow() {
+    HWND h = GetTopWindow(nullptr);
+    DWORD pid = GetCurrentProcessId();
+
+    while (h) {
+        DWORD winPid = 0;
+        GetWindowThreadProcessId(h, &winPid);
+        if (winPid == pid && IsWindowVisible(h)) {
+            return h;
+        }
+        h = GetNextWindow(h, GW_HWNDNEXT);
+    }
+    return nullptr;
+}
+
+// -------- process-wide guard --------
+
 bool AcquireProcessGuard() {
     HANDLE h = CreateMutexW(nullptr, TRUE, L"Global\\NiohModLoader_Init");
     if (!h) return true;
@@ -58,6 +74,95 @@ void Log(const std::wstring& msg) {
         }
     }
     catch (...) {}
+}
+
+// -------- overlay --------
+
+HWND gOverlay = nullptr;
+UINT_PTR gOverlayTimer = 1;
+HWND gGameHwnd = nullptr;
+
+DWORD WINAPI OverlayFollowThread(LPVOID) {
+    while (gOverlay) {
+        HWND fg = GetForegroundWindow();
+        if (fg == gGameHwnd) {
+            ShowWindow(gOverlay, SW_SHOW);
+        }
+        else {
+            ShowWindow(gOverlay, SW_HIDE);
+        }
+        Sleep(100);
+    }
+    return 0;
+}
+
+LRESULT CALLBACK OverlayWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_TIMER && wParam == gOverlayTimer) {
+        DestroyWindow(hWnd);
+        return 0;
+    }
+    if (msg == WM_DESTROY) {
+        gOverlay = nullptr;
+        return 0;
+    }
+    return DefWindowProcW(hWnd, msg, wParam, lParam);
+}
+
+void CreateOverlayWindow() {
+    WNDCLASSEXW wc{};
+    wc.cbSize = sizeof(wc);
+    wc.lpfnWndProc = OverlayWndProc;
+    wc.hInstance = GetModuleHandleW(nullptr);
+    wc.lpszClassName = L"NiohModLoaderOverlay";
+
+    RegisterClassExW(&wc);
+
+    for (int i = 0; i < 200; ++i) {
+        gGameHwnd = FindGameWindow();
+        if (gGameHwnd) break;
+        Sleep(50);
+    }
+    if (!gGameHwnd) return;
+
+    gOverlay = CreateWindowExW(
+        WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW,
+        wc.lpszClassName,
+        L"",
+        WS_POPUP,
+        10, 10, 260, 50,
+        nullptr, nullptr, wc.hInstance, nullptr
+    );
+
+    SetWindowLongPtrW(gOverlay, GWLP_HWNDPARENT, (LONG_PTR)gGameHwnd);
+    SetLayeredWindowAttributes(gOverlay, 0, 255, LWA_ALPHA);
+    ShowWindow(gOverlay, SW_SHOW);
+    SetTimer(gOverlay, gOverlayTimer, 20000, nullptr);
+
+    CreateThread(nullptr, 0, OverlayFollowThread, nullptr, 0, nullptr);
+}
+
+void DrawOverlayText(const wchar_t* text) {
+    if (!gOverlay) return;
+
+    HDC hdc = GetDC(gOverlay);
+
+    RECT r{ 0, 0, 260, 50 };
+    SetBkMode(hdc, TRANSPARENT);
+    SetTextColor(hdc, RGB(230, 230, 230));
+
+    HFONT font = CreateFontW(
+        20, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
+        DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
+        CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY,
+        DEFAULT_PITCH | FF_DONTCARE, L"Segoe UI"
+    );
+
+    HFONT old = (HFONT)SelectObject(hdc, font);
+    DrawTextW(hdc, text, -1, &r, DT_LEFT | DT_VCENTER | DT_SINGLELINE);
+    SelectObject(hdc, old);
+    DeleteObject(font);
+
+    ReleaseDC(gOverlay, hdc);
 }
 
 // -------- proxy forwarding --------
@@ -117,8 +222,7 @@ void LoadMods() {
         }
 
         for (auto& modDir : std::filesystem::directory_iterator(modsDir)) {
-            if (!modDir.is_directory())
-                continue;
+            if (!modDir.is_directory()) continue;
 
             std::filesystem::path dllPath;
 
@@ -145,13 +249,17 @@ void LoadMods() {
     }
 }
 
-
 DWORD WINAPI InitThread(LPVOID) {
     OpenLog();
     Log(L"[Loader] Session started");
+
+    CreateOverlayWindow();
+    DrawOverlayText(L"Mod Loader active");
+
     Sleep(5000);
     Log(L"[Loader] InitThread started");
     LoadMods();
+
     return 0;
 }
 
@@ -159,9 +267,7 @@ DWORD WINAPI InitThread(LPVOID) {
 
 BOOL APIENTRY DllMain(HMODULE, DWORD reason, LPVOID) {
     if (reason == DLL_PROCESS_ATTACH) {
-        if (!AcquireProcessGuard()) {
-            return TRUE;
-        }
+        if (!AcquireProcessGuard()) return TRUE;
         DisableThreadLibraryCalls(GetModuleHandle(nullptr));
         LoadReal();
         CreateThread(nullptr, 0, InitThread, nullptr, 0, nullptr);
